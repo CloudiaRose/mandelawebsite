43721875a8fe662bf263f76082d46b30c5020af5
Cloudia <cloudia.barr@live.com>
2024-04-09 13:40:18 +0200

    edited admin.py by adding docstrings

--- mandela/admin.py
+++ mandela/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-# Register your models here.
 from .models import Biography, Quote, Legacy
+
+"""
+Register Django admin classes for models Biography, Quote, and Legacy.
+"""
 
 admin.site.register(Biography)
 admin.site.register(Quote)
